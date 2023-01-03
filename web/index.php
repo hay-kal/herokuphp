23769f5af260bba68920150b269fdfd82a8d5e32
hay-kal <21040411@myrp.edu.sg>
2023-01-03 15:21:01 +0800

    Activity 3 - Doctype added

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
-<!-- fix : <html> element does not have a [lang] attribute -->
-<!-- fix : Document doesn't have a <title> element -->
+<!-- fix : Page lacks the HTML doctype, thus triggering quirks-mode. Document must contain a doctype -->
+<!DOCTYPE html>
 <html lang="en">
     <title>Activity 3 - Lighthouse Fix</title>
 <head>
