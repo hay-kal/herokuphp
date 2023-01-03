292964538254133244c1865ee8eb5b925668d579
hay-kal <21040411@myrp.edu.sg>
2023-01-03 15:12:47 +0800

    Activity 3 - Title Fix

--- web/index.php
+++ web/index.php
@@ -1,4 +1,7 @@
+<!-- fix : <html> element does not have a [lang] attribute -->
+<!-- fix : Document doesn't have a <title> element -->
 <html lang="en">
+    <title>Activity 3 - Lighthouse Fix</title>
 <head>
 </head>
 <body>
