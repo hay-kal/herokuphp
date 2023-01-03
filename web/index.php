5a663288d59fefbb70e63fc0013c96ea4fd80aab
hay-kal <21040411@myrp.edu.sg>
2023-01-03 15:39:58 +0800

    Activity 3 - viewport desc update

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@
     <!-- fix : Document doesn't have a <title> element -->
     <title>Activity 3 - Lighthouse Fix</title>
 <head>
+    <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
