839a04cfeb926a90c5987d44f8f1a24f6963d97f
hay-kal <21040411@myrp.edu.sg>
2023-01-03 15:28:34 +0800

    Activity 3 - Added meta viewport

--- web/index.php
+++ web/index.php
@@ -1,8 +1,11 @@
 <!-- fix : Page lacks the HTML doctype, thus triggering quirks-mode. Document must contain a doctype -->
 <!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
+    <!-- fix : Document doesn't have a <title> element -->
     <title>Activity 3 - Lighthouse Fix</title>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   Hello World!
